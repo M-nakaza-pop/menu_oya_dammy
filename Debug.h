9d7e70f217993bc4839d4b37bcfa2c490e303d1d
new-system-EMG <45838209+M-nakaza-pop@users.noreply.github.com>
2020-05-08 17:06:14 +0900

    Add files via upload
    Debug用ヘッダー

---
+++ Debug.h
@@ -0,0 +1,24 @@
+#ifndef __DEBUG_H__
+#define __DEBUG_H__
+
+#define DEBUG
+#ifdef DEBUG
+    #define BeginDebugPrint()    Serial.begin( 19200 )
+    #define DebugPrint( message )\
+        {\
+            char __buff__[ 512 ];\
+            sprintf( __buff__\
+                   , "%s (Func:%s, File:%s, Line:%d)"\
+                   , message\
+                   , __func__\
+                   , __FILE__\
+                   , __LINE__ );\
+            Serial.println( __buff__ );\
+            Serial.flush();\
+        }
+#else
+    #define BeginDebugPrint()
+    #define DebugPrint( message )
+#endif // DEBUG
+#endif // __DEBUG_H__				
+
